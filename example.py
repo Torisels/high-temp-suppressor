72c427aa6f64c997a79e2a118b1b7d0edf2967ca
Gustaw Daczkowski <gucio.lektor@gmail.com>
2019-12-10 08:56:05 +0100

    Update example

--- example.py
+++ example.py
@@ -3,15 +3,20 @@ import step
 from step import Stepper
 
 encoder = rotary_encoder.Encoder(12, 16)
-stepper = Stepper(step.FULL_STEP, 15, 13, 14, 2, delay=2)
+stepper = Stepper(step.FULL_STEP, 15, 13, 14, 2, 5, delay=3)
 
-while True:  # Infinite loop
-    if stepper.allowed:
-        if encoder.diff > 0:
-            print(encoder.diff)
-            stepper.step(85, 1)
-            encoder.diff -= 1
-        elif encoder.diff < 0:
-            print(encoder.diff)
-            stepper.step(85, -1)
-            encoder.diff += 1
+
+def s(x, y):
+    global stepper
+    stepper.step(x, y)
+
+# while True:  # Infinite loop
+#     if stepper.allowed:
+#         if encoder.diff > 0:
+#             print(encoder.diff)
+#             stepper.step(85, 1)
+#             encoder.diff -= 1
+#         elif encoder.diff < 0:
+#             print(encoder.diff)
+#             stepper.step(85, -1)
+#             encoder.diff += 1
