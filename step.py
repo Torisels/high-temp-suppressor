c556dc435ab20896cf5652b21cefab3d24d16ade
Torisels <gucio.lektor@gmail.com>
2019-12-10 10:40:05 +0100

    Add singleton model to stepper motor class.

--- step.py
+++ step.py
@@ -28,14 +28,29 @@ class Stepper:
         [HIGH, LOW, LOW, HIGH]
     ]
 
+    _INSTANCE = None
+    PIN1 = 15
+    PIN2 = 13
+    PIN3 = 14
+    PIN4 = 2
+    DELAY = 2
+    PIN_EMERGENCY_STOP = None
+
+    @classmethod
+    def get_instance(cls):#singleton, use this to create stepper motor instance for whole project
+        if cls._INSTANCE is None:
+            cls._INSTANCE = Stepper(cls.PIN1, cls.PIN2, cls.PIN3, cls.PIN4, cls.PIN_EMERGENCY_STOP, cls.DELAY)
+        return cls._INSTANCE
+
     def __init__(self, mode, pin1, pin2, pin3, pin4, pin_emergency_stop, delay=2):
         self.mode = mode
         self.pins = [Pin(pin1, Pin.OUT), Pin(pin2, Pin.OUT), Pin(pin3, Pin.OUT), Pin(pin4, Pin.OUT)]
         self.delay = delay
 
         self._scheduled_steps = 0
-        self.pin_e_stop = Pin(pin_emergency_stop, Pin.IN, Pin.PULL_UP)
-        self.pin_e_stop.irq(trigger=Pin.IRQ_FALLING, handler=self.handle_emergency_stop)
+        if pin_emergency_stop:
+            self.pin_e_stop = Pin(pin_emergency_stop, Pin.IN, Pin.PULL_UP)
+            self.pin_e_stop.irq(trigger=Pin.IRQ_FALLING, handler=self.handle_emergency_stop)
         self.allowed = True
         # Initialize all to 0
         self.reset()
@@ -60,7 +75,7 @@ class Stepper:
     async def loop(self):
         while True:
             if self._scheduled_steps == 0:
-                await asyncio.sleep_ms(300)
+                await asyncio.sleep_ms(200)
             elif self._scheduled_steps > 0:
                 await self.step(1, 1)
                 self._scheduled_steps -= 1
@@ -93,9 +108,9 @@ class Stepper:
             pin.off()
 
 
-def rn(times=Stepper.FULL_ROTATION_FULL_STEP, delay=2, dir=1):
-    s1 = Stepper(Stepper.FULL_STEP, 15, 13, 14, 2, delay=delay)
-    start = time.time()
-    s1.step(times, dir)
-    end = time.time()
-    print(end - start)
+# def rn(times=Stepper.FULL_ROTATION_FULL_STEP, delay=2, dir=1):
+#     s1 = Stepper(Stepper.FULL_STEP, 15, 13, 14, 2, delay=delay)
+#     start = time.time()
+#     s1.step(times, dir)
+#     end = time.time()
+#     print(end - start)
