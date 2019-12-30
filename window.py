54b53e65d942c804e58be3075ed3fa0fa111cc3e
Mikołaj Kita <m.t.kita@stud.elka.pw.edu.pl>
2019-12-30 22:32:34 +0100

    Replace window.py

--- window.py
+++ window.py
@@ -2,41 +2,40 @@
 class window():
 
     def decision(self, tin, tout, humin, humout, lightin, lightout, optimal1=21, optimal2=28):
-        # optymalna temperatura 21C - 28C
-        # optymalna wilgotnosc od 40% do 70%
-        # Zrodlo: https://www.weather.gov/media/epz/wxcalc/heatIndex.pdf
-        tin = 9 / 5 * tin + 32  # konwersja na Fahrenheita
+        # Optimal temperature is usually claimed to be between 21°C - 28°C, but there is room for customization
+        # Source: https://www.weather.gov/media/epz/wxcalc/heatIndex.pdf
+        tin = 9 / 5 * tin + 32  # Fahrenheit conversion
         tout = 9 / 5 * tout + 32
         heatin = -42.379 + 2.04901523 * tin + 10.14333127 * humin - .122475541 * tin * humin - .00683783 * tin * tin\
                  - .05481717 * humin * humin + .00122874 * tin * tin * humin + .00085282 * tin * humin * humin \
-                 - .00000199 * tin * tin * humin * humin
+                 - .00000199 * tin * tin * humin * humin # Equation for apparent temperature
 
         heatout = -42.379 + 2.04901523 * tout + 10.14333127 * humout - .122475541 * tout * humout - .00683783 * tout * tout \
              - .05481717 * humout * humout + .00122874 * tout * tout * humout + .00085282 * tout * humout * humout \
                   - .00000199 * tout * tout * humout * humout
 
-        heatin = (heatin-32)*5/9    # konwersja na Celsjusza
+        heatin = (heatin-32)*5/9    # Celsius conversion
         heatout = (heatout-32)*5/9
-        s = 'Odczuwalna temperatura w pomieszczeniu wynosi: ' + str(round(heatin, 2)) + "°C" + '\n'
+        s = 'Apparent temperature in a room equals to: ' + str(round(heatin, 2)) + "°C" + '\n'
         if optimal2 > heatin > optimal1:
             if heatin > heatout and lightout > 35000:
-                s += 'Odczuwalna temperatura znajduje sie w podanym optymalnym przedziale. Jesli chcesz, aby było chlodniej uchyl okno.'
+                s += 'Apparent temperature is located in given optimal temperature range. If you want to decrease temperature in the room, tilt the window.'
             elif heatin > heatout:
-                s += 'Odczuwalna temperatura znajduje sie w podanym optymalnym przedziale. Jesli chcesz, aby bylo chłodniej otworz okno.'
+                s += 'Apparent temperature is located in given optimal temperature range. If you want to decrease temperature in the room, open the window.'
             else:
-                s += 'Odczuwalna temperatura znajduje sie w podanym optymalnym przedziale. Jesli chcesz, aby bylo cieplej otworz okno.'
+                s += 'Apparent temperature is located in given optimal temperature range. If you want to increase temperature in the room, open the window.'
         else:
             if heatin > optimal2:
                 if heatin > heatout and lightout > 35000:
-                    s += 'Zalecamy uchylic okno w celu osiagniecia temperatury blizszej optymalnej.'
+                    s += 'It is advised to tilt the window to achieve temperature closer to optimum range.'
                 elif heatin > heatout:
-                    s += 'Zalecamy otworzyc okno w celu osiagniecia temperatury blizszej optymalnej.'
+                    s += 'It is advised to open the window achieve temperature closer to optimum range.'
                 else:
-                    s += 'Nie zalecamy otwierac okien. Będzie jeszcze cieplej.'
+                    s += 'It is not advised to open the window. It will only get hotter.'
             else:
                 if heatin < heatout:
-                    s += 'Zalecamy otworzyc okno w celu osiągniecia temperatury blizszej optymalnej.'
+                    s += 'It is advised to open the window achieve temperature closer to optimum range.'
                 else:
-                    s += 'Nie zalecamy otwierac okien. Bedzie jeszcze zimniej.'
+                    s += 'It is not advised to open the window. It will only get hotter.'
         return s
 
