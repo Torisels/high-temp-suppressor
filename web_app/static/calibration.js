e771cc07e26205d4c037a6604225110f4c03944e
Hubert Michał Decyusz <h.decyusz@stud.elka.pw.edu.pl>
2020-01-06 19:45:17 +0100

    Upload New File

---
+++ web_app/static/calibration.js
@@ -0,0 +1,29 @@
+function plus_button() { 
+    elem = document.getElementById("plus") ;
+}
+
+function minus_button() {
+    elem = document.getElementById("minus") ;
+}
+function scale1() {
+    elem = document.getElementById("1") ;
+}
+function scale2() {
+    elem = document.getElementById("2") ;
+
+}
+
+function scale3() {
+    elem = document.getElementById("3") ;
+
+}
+
+function previous_step() {
+     elem = document.getElementById("previous") ;
+
+}
+
+function previous_step() {
+    elem = document.getElementById("next") ;
+
+}
